57820714f2022a4d5d334d682b169a22394af4dc
TheshikaSamaraweera <theshikanavod@gmail.com>
2025-10-17 23:30:51 +0530

    create product service

---
+++ product-service/src/test/java/com/democode/product_service/TestcontainersConfiguration.java
@@ -0,0 +1,18 @@
+package com.democode.product_service;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.mongodb.MongoDBAtlasLocalContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+	@Bean
+	@ServiceConnection
+	MongoDBAtlasLocalContainer mongoDbAtlasContainer() {
+		return new MongoDBAtlasLocalContainer(DockerImageName.parse("mongodb/mongodb-atlas-local:latest"));
+	}
+
+}
