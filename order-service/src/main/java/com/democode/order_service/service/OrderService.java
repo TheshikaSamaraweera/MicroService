fb902f71bdfc11fdee8faea9817ed18eecbb1da8
TheshikaSamaraweera <theshikanavod@gmail.com>
2025-10-19 20:10:49 +0530

    crete order service

---
+++ order-service/src/main/java/com/democode/order_service/service/OrderService.java
@@ -0,0 +1,46 @@
+package com.democode.order_service.service;
+
+import com.democode.order_service.dto.OrderLineItemsDto;
+import com.democode.order_service.dto.OrderRequest;
+import com.democode.order_service.model.Order;
+import com.democode.order_service.model.OrderLineItems;
+import com.democode.order_service.reopsitory.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+
+
+
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
