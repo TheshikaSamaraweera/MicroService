8416a7e77ccb578f86e93172b1993d48eba89ca4
TheshikaSamaraweera <theshikanavod@gmail.com>
2025-10-19 20:11:00 +0530

    crete order repo

---
+++ order-service/src/main/java/com/democode/order_service/reopsitory/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.democode.order_service.reopsitory;
+
+import com.democode.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
